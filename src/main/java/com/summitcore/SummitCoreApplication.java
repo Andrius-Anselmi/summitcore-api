f6ec383673db6bd4589c82e525823f9363d6fd84
andrius anselmi <andriusanselmi12@gmail.com>
2026-05-20 15:41:36 -0300

    chore: initial setup

---
+++ src/main/java/com/summitcore/SummitCoreApplication.java
@@ -0,0 +1,13 @@
+package com.summitcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummitCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummitCoreApplication.class, args);
+    }
+
+}
