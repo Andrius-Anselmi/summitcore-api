bfca6b53d56c726f16933e97726c29a08c52ced6
andrius anselmi <andriusanselmi12@gmail.com>
2026-05-23 16:59:47 -0300

    feat: add @Builder in class EventEntity to use in mapper

--- src/main/java/com/summitcore/infrastructure/persistence/EventEntity.java
+++ src/main/java/com/summitcore/infrastructure/persistence/EventEntity.java
@@ -2,10 +2,7 @@ package com.summitcore.infrastructure.persistence;
 
 import com.summitcore.core.enuns.EventType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class EventEntity{
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
