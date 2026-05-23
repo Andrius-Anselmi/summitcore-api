673431cda8b276bd8554de52e9b62bdf1e12e5bd
andrius anselmi <andriusanselmi12@gmail.com>
2026-05-23 16:47:35 -0300

    feat: add static constructor for Event

--- src/main/java/com/summitcore/core/entities/Event.java
+++ src/main/java/com/summitcore/core/entities/Event.java
@@ -17,6 +17,13 @@ public record Event(
          EventType typeEvent
 ) {
 
+    public static Event create(String name, String description, String identify, String location,
+                               Integer capacity, LocalDateTime startEvent, LocalDateTime endEvent, String organizer,
+                               EventType typeEvent){
+        return new Event(null, name, description,identify,location,capacity,startEvent,endEvent,organizer,typeEvent);
+    }
+
+
 
 
 
