c57bd9d6efc5c871b079adfbb98627e8fc336a3f
andrius anselmi <andriusanselmi12@gmail.com>
2026-05-24 00:42:46 -0300

    feat: add @Component to EventRepositoryGateway and implement findAll and findById

--- src/main/java/com/summitcore/infrastructure/gateway/EventRepositoryGateway.java
+++ src/main/java/com/summitcore/infrastructure/gateway/EventRepositoryGateway.java
@@ -5,14 +5,30 @@ import com.summitcore.core.gateway.EventGateway;
 import com.summitcore.infrastructure.mapper.EventEntityMapper;
 import com.summitcore.infrastructure.persistence.EventRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
+@Component
 public class EventRepositoryGateway implements EventGateway {
 
     private final EventRepository repository;
 
+
     @Override
     public Event create(Event event) {
         return EventEntityMapper.toEvent(repository.save(EventEntityMapper.toEventEntity(event)));
+    }
+
+    @Override
+    public List<Event> findAll(){
+        return repository.findAll().stream().map(EventEntityMapper::toEvent).toList();
+    }
+
+    @Override
+    public Optional<Event> findById(Long id){
+        return repository.findById(id).map(EventEntityMapper::toEvent);
     }
 }
