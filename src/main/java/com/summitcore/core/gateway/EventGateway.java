d8a304b0d0fbbeaff154b892e87ac2bd0af2050e
andrius anselmi <andriusanselmi12@gmail.com>
2026-05-23 23:48:01 -0300

    feat: add findAll and findById methods to EventGateway

--- src/main/java/com/summitcore/core/gateway/EventGateway.java
+++ src/main/java/com/summitcore/core/gateway/EventGateway.java
@@ -2,9 +2,14 @@ package com.summitcore.core.gateway;
 
 import com.summitcore.core.entities.Event;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EventGateway {
-    
+
     Event create(Event event);
+    List<Event> findAll();
+    Optional<Event> findById(Long id);
 
 
 }
