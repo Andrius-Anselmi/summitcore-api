f80a4d9accae34697d5e24936e1811cc16c55759
andrius anselmi <andriusanselmi12@gmail.com>
2026-05-27 18:09:20 -0300

    feat: throw custom duplicate exception in use case

--- src/main/java/com/summitcore/core/useCases/CreateEventCaseImpl.java
+++ src/main/java/com/summitcore/core/useCases/CreateEventCaseImpl.java
@@ -1,18 +1,23 @@
 package com.summitcore.core.useCases;
 
 import com.summitcore.core.entities.Event;
+import com.summitcore.core.exception.DuplicateEventException;
 import com.summitcore.core.gateway.EventGateway;
 
 public class CreateEventCaseImpl implements CreateEventCase {
 
     private final EventGateway eventGateway;
 
-    public CreateEventCaseImpl (EventGateway eventGateway){
+    public CreateEventCaseImpl(EventGateway eventGateway) {
         this.eventGateway = eventGateway;
     }
 
     @Override
-    public Event execute(Event event) {
+    public Event execute(Event event) throws DuplicateEventException {
+
+        if (!(eventGateway.filterEventByIdentify(event.identify()).isEmpty())) {
+            throw new DuplicateEventException("Identify " + event.identify() + " already exists");
+        }
         return eventGateway.create(event);
     }
 }
